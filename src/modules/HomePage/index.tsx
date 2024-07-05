242ef85368db9a24ec20e545edf24caf1b6eca21
Julian Ramlal <julian@colahar.com>
2024-07-05 06:33:30 -0600

    Add basescan links to event transactions

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -3,6 +3,8 @@ import { Interface } from 'ethers';
 
 import SofamonWearablesV2ABI from 'contracts/sofamon/SofamonWearablesV2.abi.json';
 
+import { Link } from '../../common/components/Link';
+
 export const HomePage = ({ baseLogs }: { baseLogs: Array<BaseLog> }) => {
   const SofamonWearables = new Interface(SofamonWearablesV2ABI);
   const TradeEvent = SofamonWearables.getEvent('Trade') ?? '';
@@ -17,12 +19,12 @@ export const HomePage = ({ baseLogs }: { baseLogs: Array<BaseLog> }) => {
             const [trader, subject, isBuy] = SofamonWearables.decodeEventLog(TradeEvent, logEvent.data);
 
             return (
-              <li>
-                {trader} - {isBuy ? 'BUY' : 'SELL'}
+              <li key={logEvent.id}>
+                <Link href={`https://basescan.org/tx/${logEvent.transaction_hash}`}>{trader}</Link>-{' '}
+                {isBuy ? 'BUY' : 'SELL'}
               </li>
             );
           } catch (error) {
-            console.log(logEvent.data);
             return null;
           }
         })}
